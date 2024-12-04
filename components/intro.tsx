f1299f9f4e9e576a92aceaf902531ee31b57a958
Johnny Siu <johnnysiu@Johnnys-MBP.attlocal.net>
2024-12-04 10:12:34 -0800

    updated resume

--- components/intro.tsx
+++ components/intro.tsx
@@ -67,7 +67,7 @@ function Intro() {
 
                 <a
                     className="group cursor-pointer bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition borderBlack dark:bg-white/10"
-                    href="/JohnnySiu-Resume.pdf"
+                    href="/Johnny_Siu_Resume.pdf"
                     download
                 >
                     Download Resume{" "}
